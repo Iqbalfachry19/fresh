fe01171adc221ae1cdc4b1b579a19156473e9314
iqbalfachry19 <ifachry@rocketmail.com>
2022-09-03 12:45:39 +0700

    hi

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -3,32 +3,42 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import * as $0 from "./routes/[name].tsx";
-import * as $1 from "./routes/about.tsx";
-import * as $2 from "./routes/api/joke.ts";
-import * as $3 from "./routes/api/random-uuid.tsx";
-import * as $4 from "./routes/countdown.tsx";
-import * as $5 from "./routes/github/[username].tsx";
-import * as $6 from "./routes/greet/[name].tsx";
-import * as $7 from "./routes/index.tsx";
-import * as $8 from "./routes/search.tsx";
+import * as $1 from "./routes/_middleware.ts";
+import * as $2 from "./routes/about.tsx";
+import * as $3 from "./routes/api/joke.ts";
+import * as $4 from "./routes/api/random-uuid.tsx";
+import * as $5 from "./routes/countdown.tsx";
+import * as $6 from "./routes/github/[username].tsx";
+import * as $7 from "./routes/greet/[name].tsx";
+import * as $8 from "./routes/html.tsx";
+import * as $9 from "./routes/index.tsx";
+import * as $10 from "./routes/myHandler.tsx";
+import * as $11 from "./routes/plain.tsx";
+import * as $12 from "./routes/search.tsx";
 import * as $$0 from "./islands/Countdown.tsx";
 import * as $$1 from "./islands/Counter.tsx";
+import * as $$2 from "./islands/MyIsland.tsx";
 
 const manifest = {
   routes: {
     "./routes/[name].tsx": $0,
-    "./routes/about.tsx": $1,
-    "./routes/api/joke.ts": $2,
-    "./routes/api/random-uuid.tsx": $3,
-    "./routes/countdown.tsx": $4,
-    "./routes/github/[username].tsx": $5,
-    "./routes/greet/[name].tsx": $6,
-    "./routes/index.tsx": $7,
-    "./routes/search.tsx": $8,
+    "./routes/_middleware.ts": $1,
+    "./routes/about.tsx": $2,
+    "./routes/api/joke.ts": $3,
+    "./routes/api/random-uuid.tsx": $4,
+    "./routes/countdown.tsx": $5,
+    "./routes/github/[username].tsx": $6,
+    "./routes/greet/[name].tsx": $7,
+    "./routes/html.tsx": $8,
+    "./routes/index.tsx": $9,
+    "./routes/myHandler.tsx": $10,
+    "./routes/plain.tsx": $11,
+    "./routes/search.tsx": $12,
   },
   islands: {
     "./islands/Countdown.tsx": $$0,
     "./islands/Counter.tsx": $$1,
+    "./islands/MyIsland.tsx": $$2,
   },
   baseUrl: import.meta.url,
 };
--- routes/index.tsx
+++ routes/index.tsx
@@ -2,20 +2,45 @@
 import { h } from 'preact';
 import { tw } from '@twind';
 import Counter from '../islands/Counter.tsx';
-
+import Icon from 'preact-material-components/Icon';
+import { useState } from 'preact/hooks';
 export default function Home() {
+  const [search, setSearch] = useState('');
   return (
-    <div class={tw`p-4 mx-auto max-w-screen-md`}>
-      <img
-        src="/logo.svg"
-        height="100px"
-        alt="the fresh logo: a sliced lemon dripping with juice"
+    <div class={tw` mx-auto mt-40 max-w-screen-md`}>
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/icon?family=Material+Icons"
       />
+      <header
+        class={tw` fixed left-0  right-0 top-0 flex bg-blue-400 justify-center items-center `}
+      >
+        <img
+          class={tw`w-10 h-10 justify-center`}
+          src="/logogram__ktbs_white.png"
+        />
+        <div class={tw`flex  bg-blue-300 rounded-lg m-4`}>
+          <input
+            class={tw`w-[400px]  py-6 px-4 bg-blue-300 outline-none rounded-lg text-white placeholder-gray-200 h-4 justify-center `}
+            type="text"
+            placeholder="Semangat Kemerdekaan cari 'veteran'"
+            value={search}
+            onInput={(e) => setSearch((e.target as HTMLInputElement).value)}
+          />
+          <div class={tw`items-center flex mt-2 cursor-pointer text-white  `}>
+            <Icon class={tw`w-8 h-8`}>search</Icon>
+          </div>
+        </div>
+      </header>
       <p class={tw`my-6`}>
         Welcome to `fresh`. Try update this message in the ./routes/index.tsx
         file, and refresh.
       </p>
       <Counter start={3} />
+      <nav>
+        <a href="/html/">Donasi</a> |<a href="/css/">Galang Dana</a> |
+        <a href="/js/">JavaScript</a> |<a href="/python/">Python</a>
+      </nav>
     </div>
   );
 }
